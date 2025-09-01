67b070e6b30c73bbd270a2ca70b603ebf5e6f6e0
nosebleed1ng <gabrielcastillo7882@gmail.com>
2025-08-31 23:19:45 -0400

    Finalizado

--- app/src/main/java/com/interaccion/actividad12/ActivityRecibirDatos.java
+++ app/src/main/java/com/interaccion/actividad12/ActivityRecibirDatos.java
@@ -1,5 +1,6 @@
 package com.interaccion.actividad12;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +8,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 
 public class ActivityRecibirDatos extends AppCompatActivity {
+
+    private TextView tn2;
+    private TextView ta2;
+    private TextView tc2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_recibir_datos);
+        tn2 = (TextView)findViewById(R.id.tn2);
+        ta2 = (TextView)findViewById(R.id.ta2);
+        tc2 = (TextView)findViewById(R.id.tc2);
+        String dato1 = getIntent().getStringExtra("dato1");
+        String dato2 = getIntent().getStringExtra("dato2");
+        String dato3 = getIntent().getStringExtra("dato3");
+        tn2.setText(dato1);
+        ta2.setText(dato2);
+        tc2.setText(dato3);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void Volver(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/interaccion/actividad12/MainActivity.java
+++ app/src/main/java/com/interaccion/actividad12/MainActivity.java
@@ -1,6 +1,10 @@
 package com.interaccion.actividad12;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText tn1;
+    private EditText ta1;
+    private EditText tc1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        tn1 = (EditText)findViewById(R.id.tn1);
+        ta1 = (EditText)findViewById(R.id.ta1);
+        tc1 = (EditText)findViewById(R.id.tc1);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    //Método para el botón enviar
+    public void Enviar(View view){
+        Intent i = new Intent(this, ActivityRecibirDatos.class);
+        i.putExtra("dato1", tn1.getText().toString());
+        i.putExtra("dato2", ta1.getText().toString());
+        i.putExtra("dato3", tc1.getText().toString());
+        startActivity(i);
     }
 }
